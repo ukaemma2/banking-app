442e76db22c77d223a9fb500e8e2b09993c9ef53
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2025-02-05 12:02:04 +0100

    few update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 
-export default function RootLayout({ 
-  children,
-}: Readonly<{
+export default function RootLayout({children,}: Readonly<{
   children: React.ReactNode;
 }>) { 
   return (
