7314a358f73b5d010551e09e0dcec0b570e79a5f
Emmanuel O. Ukah <NGPIS05@nnpcgroup.com>
2025-10-07 10:35:17 +0100

    more updates on utiles

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,7 +16,7 @@ export const formatDateTime = (dateString: Date) => {
     day: "numeric", // numeric day of the month (e.g., '25')
     hour: "numeric", // numeric hour (e.g., '8')
     minute: "numeric", // numeric minute (e.g., '30')
-    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false) more changes
   };
 
   const dateDayOptions: Intl.DateTimeFormatOptions = {
@@ -25,6 +25,7 @@ export const formatDateTime = (dateString: Date) => {
     month: "2-digit", // abbreviated month name (e.g., 'Oct')
     day: "2-digit", // numeric day of the month (e.g., '25')
   };
+  
 
   const dateOptions: Intl.DateTimeFormatOptions = {
     month: "short", // abbreviated month name (e.g., 'Oct')
